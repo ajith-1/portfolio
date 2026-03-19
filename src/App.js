8d833dd64a3364e6e5dc739acbde0f56889539e1
null <null>
2026-03-19 14:50:03 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import { useState, useEffect, useRef } from "react";
-import Portfolio from "./Portfolio";
-import AjithKumar from "./AjithKumar";
-import AjithKumarM from "./AjithKumarM";
 import Newlook from "./Newlook";
-
-
-
 
 export default function App() {
     return (
 <>
-{/* <Portfolio/> */}
-{/* <AjithKumar/> */}
-{/* <AjithKumarM/> */}
 <Newlook/>
-
 </>
 
     );
